172c1e8334fa6d98489e99a837b34be1e31b8450
Danial Fitri <48023732+dfx81@users.noreply.github.com>
2022-02-14 00:36:45 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,16 @@
 module.exports = {
-  mongoURI: "mongodb+srv://admin:iVZXDsUEi6jIDtHe@cluster0.osqdy.mongodb.net/dsara",
-  secretOrKey: "secret",
-  owner: "61f14f05f573ebef71b7da17",
-  stripeKey: "sk_test_51KSFBjKbcAh5zYUdv4GgRHLpaFC1WRWK3PJSUy6AiehTH6uxx9VMhKSYhoodF8HGTAjDCa1tqtkKZVj7MDCEeA4f00cpRlWxxn",
-  domain: "http://localhost:8080",
+  mongoURI: "[uri]",
+  secretOrKey: "[secret]",
+  owner: "[owner]",
+  stripeKey: "[key]",
+  domain: "[domain]",
   mailConfig: {
     host: "smtp.ethereal.email",
     mailport: 587,
     auth: {
-      user: "gustave.leuschke52@ethereal.email",
-      pass: "dbbQdpJjJXABy29HeD"
+      user: "[email]",
+      pass: "[pass]"
     }
   },
-  stripeEndpoint: "whsec_cf5c2dca3c303f2320b6a982263bfcb3c9168c9013e89147372dc2c6451763fc"
+  stripeEndpoint: "[endpoint]"
 };
